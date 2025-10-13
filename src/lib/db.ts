2e6314232c855968d076863a6975c416f6223d18
sushantg <sushantg@vellestudio.com>
2025-10-13 22:37:44 +0530

    02: database

---
+++ src/lib/db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@/generated/prisma";
+
+const globalForPrism = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma = globalForPrism.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrism.prisma = prisma;
+}
+
+export default prisma;
